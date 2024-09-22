3da53f730962e990309677161b49c04594340301
rsteimen <rsteimen@radynamics.com>
2024-09-22 16:39:39 +0200

    move AmendmentsView.createRow into new class AmendmentRowView

--- src/main/java/com/radynamics/xrplservermgr/ui/AmendmentsView.java
+++ src/main/java/com/radynamics/xrplservermgr/ui/AmendmentsView.java
@@ -40,79 +40,9 @@ public class AmendmentsView extends JPanel {
                 .collect(Collectors.toList());
         for (var f : sorted) {
             var a = knownAmendments.stream().filter(o -> o.hash().equals(f.hash())).findFirst().orElse(null);
-            pnl.add(createRow(a, f));
+            var v = new AmendmentRowView(a, f);
+            pnl.add(v.view());
         }
-    }
-
-    private Component createRow(Amendment a, Feature f) {
-        var l = new SpringLayout();
-        var p = new JPanel();
-        p.setPreferredSize(new Dimension(820, 25));
-        p.setLayout(l);
-
-        final var LEFT_NAME = 450;
-        final var LEFT_INTRODUCED = LEFT_NAME + 200;
-        final var LEFT_YEA = LEFT_INTRODUCED + 70;
-        final var LEFT_NAY = LEFT_YEA + 50;
-        {
-            var lbl = Utils.formatAsLabel(new JTextField(f.hash()));
-            p.add(lbl);
-            l.putConstraint(SpringLayout.WEST, lbl, 0, SpringLayout.WEST, p);
-            l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, p);
-        }
-        {
-            var lbl = Utils.formatAsLabel(new JTextField(f.name()));
-            p.add(lbl);
-            l.putConstraint(SpringLayout.WEST, lbl, LEFT_NAME, SpringLayout.WEST, p);
-            l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, p);
-        }
-        {
-            var lbl = Utils.formatAsLabel(new JTextField(a == null || a.introduced() == null ? "" : a.introduced().toString()));
-            p.add(lbl);
-            l.putConstraint(SpringLayout.WEST, lbl, LEFT_INTRODUCED, SpringLayout.WEST, p);
-            l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, p);
-        }
-
-        if (!f.votingActive()) {
-            var lbl = new JLabel(f.enabled() ? "enabled" : "obsolete");
-            p.add(lbl);
-            l.putConstraint(SpringLayout.WEST, lbl, LEFT_YEA, SpringLayout.WEST, p);
-            l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, p);
-            return p;
-        }
-
-        var vs = new VoteState(f);
-        state.add(vs);
-
-        var yea = new JToggleButton("Yea");
-        var nay = new JToggleButton("Nay");
-        {
-            p.add(yea);
-            yea.addActionListener(e -> {
-                // Untoggling is not supported due there must be a vote (a persisted vote cannot be unset).
-                yea.setSelected(true);
-                vs.current(Vote.accept);
-            });
-            yea.addActionListener(createVoteActionListener(yea, nay));
-            yea.setSelected(f != null && f.vetoed() != null && !f.vetoed());
-            formatButton(yea, Color.GREEN, Consts.ColorLightGreen);
-            l.putConstraint(SpringLayout.WEST, yea, LEFT_YEA, SpringLayout.WEST, p);
-            l.putConstraint(SpringLayout.NORTH, yea, 0, SpringLayout.NORTH, p);
-        }
-        {
-            p.add(nay);
-            nay.addActionListener(e -> {
-                // Untoggling is not supported due there must be a vote (a persisted vote cannot be unset).
-                nay.setSelected(true);
-                vs.current(Vote.reject);
-            });
-            nay.addActionListener(createVoteActionListener(nay, yea));
-            nay.setSelected(f != null && f.vetoed() != null && f.vetoed());
-            formatButton(nay, Color.RED, Consts.ColorLightRed);
-            l.putConstraint(SpringLayout.WEST, nay, LEFT_NAY, SpringLayout.WEST, p);
-            l.putConstraint(SpringLayout.NORTH, nay, 0, SpringLayout.NORTH, p);
-        }
-        return p;
     }
 
     private static ActionListener createVoteActionListener(JToggleButton source, JToggleButton other) {
@@ -129,5 +59,89 @@ public class AmendmentsView extends JPanel {
 
     public List<VoteState> changes() {
         return state.stream().filter(VoteState::changed).collect(Collectors.toList());
+    }
+
+    private class AmendmentRowView {
+        private final JPanel view;
+        private final JToggleButton yea = new JToggleButton("Yea");
+        private final JToggleButton nay = new JToggleButton("Nay");
+
+        public AmendmentRowView(Amendment a, Feature f) {
+            var l = new SpringLayout();
+            view = new JPanel();
+            view.setPreferredSize(new Dimension(820, 25));
+            view.setLayout(l);
+
+            final var LEFT_NAME = 450;
+            final var LEFT_INTRODUCED = LEFT_NAME + 200;
+            final var LEFT_YEA = LEFT_INTRODUCED + 70;
+            final var LEFT_NAY = LEFT_YEA + 50;
+            {
+                var lbl = Utils.formatAsLabel(new JTextField(f.hash()));
+                view.add(lbl);
+                l.putConstraint(SpringLayout.WEST, lbl, 0, SpringLayout.WEST, view);
+                l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, view);
+            }
+            {
+                var lbl = Utils.formatAsLabel(new JTextField(f.name()));
+                view.add(lbl);
+                l.putConstraint(SpringLayout.WEST, lbl, LEFT_NAME, SpringLayout.WEST, view);
+                l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, view);
+            }
+            {
+                var lbl = Utils.formatAsLabel(new JTextField(a == null || a.introduced() == null ? "" : a.introduced().toString()));
+                view.add(lbl);
+                l.putConstraint(SpringLayout.WEST, lbl, LEFT_INTRODUCED, SpringLayout.WEST, view);
+                l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, view);
+            }
+
+            if (!f.votingActive()) {
+                var lbl = new JLabel(f.enabled() ? "enabled" : "obsolete");
+                view.add(lbl);
+                l.putConstraint(SpringLayout.WEST, lbl, LEFT_YEA, SpringLayout.WEST, view);
+                l.putConstraint(SpringLayout.NORTH, lbl, 0, SpringLayout.NORTH, view);
+            }
+
+            var vs = new VoteState(f);
+            state.add(vs);
+
+            {
+                view.add(yea);
+                yea.setVisible(f.votingActive());
+                yea.addActionListener(e -> {
+                    // Untoggling is not supported due there must be a vote (a persisted vote cannot be unset).
+                    yea.setSelected(true);
+                    vs.current(Vote.accept);
+                });
+                yea.addActionListener(createVoteActionListener(yea, nay));
+                yea.setSelected(f != null && f.vetoed() != null && !f.vetoed());
+                formatButton(yea, Color.GREEN, Consts.ColorLightGreen);
+                l.putConstraint(SpringLayout.WEST, yea, LEFT_YEA, SpringLayout.WEST, view);
+                l.putConstraint(SpringLayout.NORTH, yea, 0, SpringLayout.NORTH, view);
+            }
+            {
+                view.add(nay);
+                nay.setVisible(f.votingActive());
+                nay.addActionListener(e -> {
+                    // Untoggling is not supported due there must be a vote (a persisted vote cannot be unset).
+                    nay.setSelected(true);
+                    vs.current(Vote.reject);
+                });
+                nay.addActionListener(createVoteActionListener(nay, yea));
+                nay.setSelected(f != null && f.vetoed() != null && f.vetoed());
+                formatButton(nay, Color.RED, Consts.ColorLightRed);
+                l.putConstraint(SpringLayout.WEST, nay, LEFT_NAY, SpringLayout.WEST, view);
+                l.putConstraint(SpringLayout.NORTH, nay, 0, SpringLayout.NORTH, view);
+            }
+        }
+
+        public Component view() {
+            return view;
+        }
+
+        public void setEnabled(boolean enabled) {
+            yea.setEnabled(enabled);
+            nay.setEnabled(enabled);
+        }
     }
 }
